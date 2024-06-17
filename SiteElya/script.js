6b729fd563dbd9da8b045897afcefddc4d58bf3f
Nakrime <59014748+Nakrime@users.noreply.github.com>
2024-06-17 19:53:46 +0200

    Update script.js

--- SiteElya/script.js
+++ SiteElya/script.js
@@ -1,1 +1,9 @@
-document.write(5 + 6);
+var x = 1;
+var bgImage = document.getElementById('about-1');
+
+function move(){
+  x++;
+  bgImage.style.backgroundPositionX = x + "px";
+}
+
+setInterval(move, 50);
