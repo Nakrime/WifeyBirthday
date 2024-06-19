ebade28cc88594ee92db512cb036ace7cd651c0f
Nakrime <59014748+Nakrime@users.noreply.github.com>
2024-06-19 20:17:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function move(){
 }
 setInterval(move, 50);
 
-var b = document.getElementById("NoButton");
 function ClickNo()
 {
-    var i = Math.floor(Math.random()*500)+1;
-    var j = Math.floor(Math.random()*500)+1;
-    b.style.left = i+"px";
-    b.style.top = j+"px";
+  var b = document.getElementById("NoButton");
+  var i = Math.floor(Math.random()*500)+1;
+  var j = Math.floor(Math.random()*500)+1;
+  b.style.left = i+"px";
+  b.style.top = j+"px";
 }
