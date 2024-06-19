951102021a01cf0f0a27249b291cc699cd899bc7
Nakrime <59014748+Nakrime@users.noreply.github.com>
2024-06-19 20:50:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ setInterval(move, 50);
 
 function ClickNo()
 {
-  var a = document.getElementById("YesButton");
   var b = document.getElementById("NoButton");
-  b.style.width = b.style.width - 50 + 'px'
-  b.style.height = b.style.height - 50 + 'px'
-  a.style.width = a.style.width + 50 + 'px'
-  a.style.height = a.style.height + 50 + 'px'
+  b.parentNode.removeChild(b);
 }
